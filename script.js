a22273241b2f3e9cbb310d8be3f3c076def0acfd
Phạm Bảo Hưng <nhoxhungvnq2@gmail.com>
2023-04-13 13:59:07 +0700

    Add files via upload

--- script.js
+++ script.js
@@ -1,115 +1,160 @@
 async function getSchedule() {
-    const apiUrl = "https://ergast.com/api/f1/current.json";
-    const xhr = new XMLHttpRequest();
-    xhr.open("GET", apiUrl);
-    xhr.onload = function () {
-        if (xhr.status === 200) {
-            var data = JSON.parse(xhr.responseText);
-            var races = data.MRData.RaceTable.Races;
-            // console.log(data.MRData.RaceTable.Races);
-            races.forEach((element) => {
-
-                showNextRace(element)
-            });
-
-        } else {
-            console.error(xhr.statusText);
+    let data = localStorage.getItem('scheduleData');
+    if (data) {
+        showRaces(JSON.parse(data));
+    } else {
+        try {
+            const apiUrl = "https://ergast.com/api/f1/current.json";
+            const response = await fetch(apiUrl);
+            if (response.ok) {
+                const data = await response.json();
+                localStorage.setItem('scheduleData', JSON.stringify(data));
+                showRaces(data);
+            } else {
+                console.error(response.statusText);
+            }
+        } catch (error) {
+            console.error("An error occurred while fetching the data:", error);
         }
-    };
-
-    xhr.onerror = function () {
-        console.error("An error occurred while fetching the data.");
-    };
-
-    xhr.send();
+    }
 }
+
+function showRaces(data) {
+    const races = data.MRData.RaceTable.Races;
+    races.forEach((element) => {
+        showNextRace(element)
+    });
+}
+
+
 
 async function getDriverStanding() {
-    const apiUrl = "https://ergast.com/api/f1/current/driverStandings.json";
-    const xhr = new XMLHttpRequest();
-    xhr.open("GET", apiUrl);
-    xhr.onload = function () {
-        if (xhr.status === 200) {
-            var data = JSON.parse(xhr.responseText);
-            var standing = data.MRData.StandingsTable.StandingsLists[0].DriverStandings;
-            for (item in standing) {
-                var i = standing[item];
-                // console.log(i);
-                var html = `
-                    <tr id="tb" class="sa">
-                        <td id="round">${i.position}</td>
-                        <td id="time">${i.Driver.givenName} ${i.Driver.familyName} (${i.Driver.code}${i.Driver.permanentNumber})</td>
-                        <td>${i.Driver.nationality}</td>
-                        <td id="location">${i.Driver.permanentNumber}</td>
-                        <td id="name">${i.points}</td>
-                        <td id="win">${i.wins}</td>
-                    </tr >
-                `
-                document.getElementById("driverStanding").insertAdjacentHTML("beforeend", html);
+    const localStorageKey = 'driverStanding';
+    let data = localStorage.getItem(localStorageKey);
+
+    if (!data) {
+        try {
+            const apiUrl = "https://ergast.com/api/f1/current/driverStandings.json";
+            const response = await fetch(apiUrl);
+            if (response.ok) {
+                data = await response.json();
+                localStorage.setItem(localStorageKey, JSON.stringify(data));
+            } else {
+                console.error(`API error: ${response.status} ${response.statusText}`);
+                return;
             }
-
-        } else {
-            console.error(xhr.statusText);
+        } catch (error) {
+            console.error("An error occurred while fetching the data.", error);
+            return;
         }
-    };
+    } else {
+        data = JSON.parse(data);
+    }
 
-    xhr.onerror = function () {
-        console.error("An error occurred while fetching the data.");
-    };
+    const standing = data.MRData.StandingsTable.StandingsLists[0].DriverStandings;
+    const html = standing.map((item) => `
+      <tr id="tb" class="sa">
+        <td id="round">${item.position}</td>
+        <td id="time">${item.Driver.givenName} ${item.Driver.familyName} (${item.Driver.code}${item.Driver.permanentNumber})</td>
+        <td>${item.Driver.nationality}</td>
+        <td id="location">${item.Driver.permanentNumber}</td>
+        <td id="name">${item.points}</td>
+        <td id="win">${item.wins}</td>
+      </tr>
+    `).join('');
 
-    xhr.send();
+    document.getElementById("driverStanding").innerHTML = html;
 }
+
 
 async function getConstructorStanding() {
-    const apiUrl = "https://ergast.com/api/f1/current/constructorStandings.json";
-    const xhr = new XMLHttpRequest();
-    xhr.open("GET", apiUrl);
-    xhr.onload = function () {
-        if (xhr.status === 200) {
-            var data = JSON.parse(xhr.responseText);
-            var standing = data.MRData.StandingsTable.StandingsLists[0].ConstructorStandings;
-            for (item in standing) {
-                var i = standing[item];
-                var html = `
-                    <tr id="tb" class="sa">
-                        <td id="round">${i.position}</td>
-                        <td id="time">${i.Constructor.name}</td>
-                        <td>${i.Constructor.nationality}</td>
-                        <td id="name">${i.points}</td>
-                        <td id="win">${i.wins}</td>
-                    </tr >
-                `
-                document.getElementById("constructorStanding").insertAdjacentHTML("beforeend", html);
+    const localStorageKey = 'constructorStanding';
+    let data = localStorage.getItem(localStorageKey);
+
+    if (!data) {
+        try {
+            const apiUrl = "https://ergast.com/api/f1/current/constructorStandings.json";
+            const response = await fetch(apiUrl);
+            if (response.ok) {
+                data = await response.json();
+                localStorage.setItem(localStorageKey, JSON.stringify(data));
+            } else {
+                console.error(`API error: ${response.status} ${response.statusText}`);
+                return;
             }
-
-        } else {
-            console.error(xhr.statusText);
+        } catch (error) {
+            console.error("An error occurred while fetching the data.", error);
+            return;
         }
-    };
+    } else {
+        data = JSON.parse(data);
+    }
 
-    xhr.onerror = function () {
-        console.error("An error occurred while fetching the data.");
-    };
+    data = JSON.parse(data);
+    const standing = data.MRData.StandingsTable.StandingsLists[0].ConstructorStandings;
+    const html = standing.map((item) => `
+      <tr id="tb" class="sa">
+        <td id="round">${item.position}</td>
+        <td id="time">${item.Constructor.name}</td>
+        <td>${item.Constructor.nationality}</td>
+        <td id="name">${item.points}</td>
+        <td id="win">${item.wins}</td>
+      </tr>
+    `).join('');
 
-    xhr.send();
+    document.getElementById("constructorStanding").innerHTML = html;
 }
 
+
 function showNextRace(item) {
-    var index = 0;
+    let index = 0;
     if (Date.now() <= new Date(item.date)) {
-        var html = `
+        try {
+            const raceDate = new Date(item.date + " " + item.time);
+        } catch (error) {
+            console.error(`An error occurred while parsing the date for race ${item.raceName}: ${error.message}`);
+            return;
+        }
+        const html = `
             <tr id="tb${index}" class="sa">
                 <td id="round">${item.round}</td>
                 <td id="time">${new Date(item.date + " " + item.time)}</td>
                 <td>${item.Circuit.circuitName}</td>
                 <td id="location">${item.Circuit.Location.locality}, ${item.Circuit.Location.country}</td>
                 <td id="name">${item.raceName}</td>
-            </tr >
-            `
+            </tr>
+            `;
         document.getElementById("futureRaces").insertAdjacentHTML("beforeend", html);
-        if (index == 0)
-            document.getElementById("tb0").style.color = "red";
-        index++
+
+        if (index === 0) {
+            const timeElement = document.querySelector('#tb0 #time');
+            timeElement.style.color = "red";
+
+            if (!document.getElementById("demo")) {
+                const demoElement = document.createElement('p');
+                demoElement.setAttribute("id", "demo");
+                demoElement.style.color = "black";
+                demoElement.style.fontWeight = "bold";
+                timeElement.appendChild(demoElement);
+                showTimer(item.date + " " + item.time);
+            }
+        }
+        index++;
+    }
+}
+
+
+var hours = 24;
+
+var now = new Date().getTime();
+
+var setupTime = localStorage.getItem('setupTime');
+if (setupTime == null) {
+    localStorage.setItem('setupTime', now)
+} else {
+    if (now - setupTime > hours * 60 * 60 * 1000) {
+        localStorage.clear()
+        localStorage.setItem('setupTime', now);
     }
 }
 
@@ -117,3 +162,34 @@ getSchedule();
 getDriverStanding();
 getConstructorStanding();
 
+function showTimer(date) {
+    // Set the date we're counting down to
+    var countDownDate = new Date(date).getTime();
+
+    // Update the count down every 1 second
+    var x = setInterval(function () {
+
+        // Get today's date and time
+        var now = new Date().getTime();
+
+        // Find the distance between now and the count down date
+        var distance = countDownDate - now;
+
+        // Time calculations for days, hours, minutes and seconds
+        var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+        var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+        var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+        var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+        // Display the result in the element with id="demo"
+        document.getElementById("demo").innerHTML = days + "d " + hours + "h "
+            + minutes + "m " + seconds + "s ";
+
+        // If the count down is finished, write some text
+        if (distance < 0) {
+            clearInterval(x);
+            document.getElementById("demo").innerHTML = "EXPIRED";
+        }
+    }, 1000);
+
+}
